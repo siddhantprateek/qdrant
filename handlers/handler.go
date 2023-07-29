b9829cc16194882210d668eff9db686e728fd38d
siddhantprateek <siddhantprateek@gmail.com>
2023-07-29 12:49:49 +0530

    ft(handlers): api handler to qdrant db

--- handlers/handler.go
+++ handlers/handler.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GetAllCollection(c *fiber.Ctx) error {
-	collectionsClient, ctx, cancel := db.QdrantDBConn()
+	_, collectionsClient, ctx, cancel := db.QdrantDBConn()
 	defer cancel()
 
 	r, err := collectionsClient.List(ctx, &pb.ListCollectionsRequest{})
@@ -23,4 +23,66 @@ func GetAllCollection(c *fiber.Ctx) error {
 		"message": "Qdrant Collections",
 		"data":    r.GetCollections(),
 	})
+}
+
+func CreateCollection(c *fiber.Ctx) error {
+	type NewCollection struct {
+		CollectionName string `json:"collectionName"`
+	}
+	newCollectionReq := new(NewCollection)
+
+	if err := c.BodyParser(newCollectionReq); err != nil {
+		return c.JSON(fiber.Map{
+			"message": fiber.ErrBadRequest,
+			"status":  fiber.StatusBadRequest,
+		})
+	}
+
+	result, err := db.CreateQdCollection(newCollectionReq.CollectionName)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": "Failed to create Collection: " + err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": result,
+		"status":  fiber.StatusOK,
+	})
+}
+
+func CreateField(c *fiber.Ctx) error {
+	type CreateFieldReq struct {
+		CollectionName string `json:"collectionName"`
+		FieldName      string `json:"fieldName"`
+	}
+
+	newFieldBody := new(CreateFieldReq)
+	if err := c.BodyParser(newFieldBody); err != nil {
+		return c.JSON(fiber.Map{
+			"message": fiber.ErrBadRequest,
+			"status":  fiber.StatusBadRequest,
+		})
+	}
+
+	result, err := db.CreateFieldIndex(newFieldBody.CollectionName, newFieldBody.FieldName)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": "Failed to create Field: " + err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"message": result,
+		"status":  fiber.StatusOK,
+	})
+}
+
+// Insert vectors into a collection
+func AddVectorData(c *fiber.Ctx) error {
+	return nil
+}
+
+func DeleteVectorData(c *fiber.Ctx) error {
+	return nil
 }
